34f31be187b05d93ce959f1991e224ade03b6fd9
Esfand <esfand@wow.com>
2013-02-05 16:02:04 +0200

    Insignificantly modified file formats.

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,31 +1,13 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package example;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+//import java.io.IOException;
+//import java.net.InetSocketAddress;
 
-import org.apache.avro.ipc.SocketServer;
-import org.apache.avro.ipc.SocketTransceiver;
-import org.apache.avro.ipc.specific.SpecificRequestor;
-import org.apache.avro.ipc.specific.SpecificResponder;
-import org.apache.avro.util.Utf8;
+//import org.apache.avro.ipc.SocketServer;
+//import org.apache.avro.ipc.SocketTransceiver;
+//import org.apache.avro.ipc.specific.SpecificRequestor;
+//import org.apache.avro.ipc.specific.SpecificResponder;
+//import org.apache.avro.util.Utf8;
 
 import example.proto.Mail;
 import example.proto.Message;
@@ -43,16 +25,17 @@ import java.net.InetSocketAddress;
  * Start a server, attach a client, and send a message.
  */
 public class Main {
+
     public static class MailImpl implements Mail {
         // in this simple example just return details of the message
+
         public Utf8 send(Message message) {
             System.out.println("Sending message");
             return new Utf8("Sending message to " + message.getTo().toString()
-                    + " from " + message.getFrom().toString()
-                    + " with body " + message.getBody().toString());
+                          + " from " + message.getFrom().toString()
+                          + " with body " + message.getBody().toString());
         }
     }
-
     private static Server server;
 
     private static void startServer() throws IOException {
